edf4161f44476c3d17fe61f64935bfbd20834196
ibnebattuta <ibnebattuta@duck.com>
2024-07-26 01:23:13 +0530

    update book-copy commands

--- src/command/bookCopy/AddBookCopyCommand.java
+++ src/command/bookCopy/AddBookCopyCommand.java
@@ -1,9 +1,9 @@
 package command.bookCopy;
 
-import java.util.List;
-
 import command.Command;
 import service.BookService;
+
+import java.util.List;
 
 public class AddBookCopyCommand implements Command {
     private final BookService _bookService;
@@ -15,12 +15,12 @@ public class AddBookCopyCommand implements Command {
     @Override
     public void execute(List<String> args) throws Exception {
         if (args.size() != 2) {
-            throw new IllegalArgumentException("Usage: ADD-BOOK-COPY [isbn] [branchId]");
+            throw new IllegalArgumentException("Usage: ADD-BOOK-COPY [isbn] [branchCode]");
         }
 
         var isbn = args.get(0);
-        var branchId = args.get(1);
+        var branchCode = args.get(1);
 
-        _bookService.addBookCopy(isbn, branchId);
+        _bookService.addBookCopy(isbn, branchCode);
     }
 }
--- src/command/bookCopy/CheckoutBookCopyCommand.java
+++ src/command/bookCopy/CheckoutBookCopyCommand.java
@@ -1,9 +1,9 @@
 package command.bookCopy;
 
-import java.util.List;
-
 import command.Command;
 import service.ReservationService;
+
+import java.util.List;
 
 public class CheckoutBookCopyCommand implements Command {
     private final ReservationService _reservationService;
@@ -15,13 +15,13 @@ public class CheckoutBookCopyCommand implements Command {
     @Override
     public void execute(List<String> args) throws Exception {
         if (args.size() != 3) {
-            throw new IllegalArgumentException("Usage: CHECKOUT-BOOK-COPY [patronId] [bookCopyId] [branchId]");
+            throw new IllegalArgumentException("Usage: CHECKOUT-BOOK-COPY [patronId] [bookCopyId] [branchCode]");
         }
 
         var patronId = args.get(0);
         var bookCopyId = args.get(1);
-        var branchId = args.get(2);
+        var branchCode = args.get(2);
 
-        _reservationService.checkoutBook(patronId, bookCopyId, branchId);
+        _reservationService.checkoutBook(patronId, bookCopyId, branchCode);
     }
 }
