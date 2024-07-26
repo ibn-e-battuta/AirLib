917c938f057e7f39997dd3aede833a5f9c7fd534
ibnebattuta <ibnebattuta@duck.com>
2024-07-26 15:49:48 +0530

    add abstract base classes

---
+++ src/model/base/BaseModel.java
@@ -0,0 +1,27 @@
+package model.base;
+
+import java.util.Objects;
+
+public abstract class BaseModel {
+    protected final String id;
+
+    protected BaseModel(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseModel baseModel)) return false;
+        return Objects.equals(id, baseModel.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
